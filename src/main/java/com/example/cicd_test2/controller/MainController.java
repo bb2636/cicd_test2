060a51d4af665dceaad615f17f0ab1c9a56dafc1
bb2636 <leesubin0322@gmail.com>
2025-06-23 16:32:56 +0900

    cicd auto

---
+++ src/main/java/com/example/cicd_test2/controller/MainController.java
@@ -0,0 +1,12 @@
+package com.example.cicd_test2.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainController {
+    @GetMapping
+    public String hello() {
+        return "this is cicd test 2 === auto deployment with github actions";
+    }
+}
---
+++ src/test/java/com/example/cicd_test2/CicdTest2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cicd_test2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CicdTest2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
