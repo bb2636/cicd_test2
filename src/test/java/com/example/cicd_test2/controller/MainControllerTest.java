58d179fb6ed2c3ab42c169238dbf3a63c1ffb7e9
bb2636 <leesubin0322@gmail.com>
2025-06-24 11:36:11 +0900

    test file 추가

---
+++ src/test/java/com/example/cicd_test2/controller/MainControllerTest.java
@@ -0,0 +1,17 @@
+package com.example.cicd_test2.controller;
+
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class MainControllerTest {
+
+    @Test
+    void hello() {
+        Assertions.assertThat(true).isTrue();
+    }
+}
