86b232f891e837507e30aa87ff422bf79daf943b
bb2636 <leesubin0322@gmail.com>
2025-06-24 11:37:55 +0900

    test file 강제 오류

--- src/test/java/com/example/cicd_test2/controller/MainControllerTest.java
+++ src/test/java/com/example/cicd_test2/controller/MainControllerTest.java
@@ -1,17 +1,14 @@
 package com.example.cicd_test2.controller;
 
-
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class MainControllerTest {
 
     @Test
     void hello() {
-        Assertions.assertThat(true).isTrue();
+//        Assertions.assertThat(true).isTrue();
+        throw new RuntimeException("강제 오류 발생");
     }
 }
